3e6d70cc70686fc926cbc04687e40a8ac8080dfc
Spider Tutorial <spidertutorial@outlook.com>
2016-06-12 10:46:53 -0400

    fix starting app error

--- startApp.sh
+++ startApp.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+export NODE_ENV=production
+export DB_PRD_HOST=dellstore2.cgffdp2lclff.us-west-2.rds.amazonaws.com
+export DB_PRD_USER=spidertutorial
+export DB_PRD_PASS=spidertutorial
+export NODE_HOST=localhost
+export NODE_PORT=8080
 node /myapp/index.js
