566a70241d6d15f851ae81929cea39b140618372
Manuj Aggarwal <aggarwalmanuj@users.noreply.github.com>
2016-09-10 14:34:36 -0700

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cgffdp2lclff.us-west-2.rds.amazonaws.com
+export DB_PRD_HOST=dellstore2.us-west-2.rds.amazonaws.com
 export DB_PRD_USER=spidertutorial
 export DB_PRD_PASS=spidertutorial
 export NODE_HOST=localhost
